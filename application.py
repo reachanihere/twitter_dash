710d0d8d4281c8a2704b35094af7ebf649a54a5b
Fabio Palliparambil <fabiolearning1998@gmail.com>
2021-04-25 20:54:36 +0100

    as

--- application.py
+++ application.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request
 import plotly
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import re
 import json
 import pandas as pd
-import nltk
+import hashtag
 
 
 application = Flask(__name__)
@@ -25,7 +24,7 @@ def homepage():
     sentiment_general = "positive"
     sentiment_vaccination = "positive"
     sentiment_restriction = "positive"
-    plot_general = create_plot(df_hash_tag)
+    plot_general = hashtag.create_plot(hashtag.df_hash_tag)
 
     return render_template("index.html",sentiment_general=sentiment_general,
                            sentiment_vaccination=sentiment_vaccination,
@@ -33,52 +32,6 @@ def homepage():
                            plot_general=plot_general)
 
 
-"""
-Function for collecting all the Hashtag
-"""
-
-
-def collectHashtag(x):
-    hashtags = []
-    for i in x:
-        ht = re.findall(r"#(\w+)", i)
-        hashtags.append(ht)
-    return hashtags
-
-
-"""
-Function for collecting hashed into multiple dataframe
-"""
-
-def hashtag(df_general):
-
-    HT_general = collectHashtag(df_general['text'])
-    HT_general = sum(HT_general,[])
-    general_tag = nltk.FreqDist(HT_general)
-    hashtag_general = pd.DataFrame({'Hashtag':list(general_tag.keys()),'Count':list(general_tag.values())})
-    hashtag_general = hashtag_general.nlargest(columns='Count', n=10)
-
-    return hashtag_general
-
-
-def create_plot(df_general_hashtag):
-
-    # Create a trace
-    scatter_general = [go.Scatter(
-        x=df_general_hashtag['Hashtag'],
-        y=df_general_hashtag['Count'],
-        mode='lines+markers',
-        name='lines+markers',
-        marker_color = 'rgba(255, 182, 193, .9)'
-        # mode='markers'
-    )]
-
-    general_JSON = json.dumps(scatter_general, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return general_JSON
-
-
-df_hash_tag = hashtag(df_general)
 
 
 if __name__ == '__main__':
