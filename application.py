c80a86311439dd6b81982d69d67ef102a6ef33ce
Fabio Palliparambil <fabiolearning1998@gmail.com>
2021-04-25 21:17:13 +0100

    test

--- application.py
+++ application.py
@@ -6,13 +6,10 @@ import json
 import pandas as pd
 import hashtag
 
-
 application = Flask(__name__)
 
-
-df_general = pd.read_csv('https://raw.githubusercontent.com/FabioPalliparambil98/covid-dataset/main/combined_generaltweets.csv')
-
-
+df_general = pd.read_csv(
+    'https://raw.githubusercontent.com/FabioPalliparambil98/covid-dataset/main/combined_generaltweets.csv')
 
 """
 It displays the Home Pages of the Visualisations.
@@ -24,14 +21,25 @@ def homepage():
     sentiment_general = "positive"
     sentiment_vaccination = "positive"
     sentiment_restriction = "positive"
-    plot_general = hashtag.create_plot(hashtag.df_hash_tag)
 
-    return render_template("index.html",sentiment_general=sentiment_general,
+    plot_general, plot_restriction, plot_vaccination, scatter_general, scatter_restriction, scatter_vaccination, \
+    all, actual_scatter, pie = hashtag.create_plot(hashtag.covid_general_hashtag,
+                                                   hashtag.covid_restriction_hashtag,
+                                                   hashtag.covid_vaccination_hashtag)
+
+    return render_template("index.html",
+                           plot1=plot_general,
+                           plot2=plot_vaccination,
+                           plot3=plot_restriction,
+                           scatter1=scatter_general,
+                           scatter2=scatter_restriction,
+                           scatter3=scatter_vaccination,
+                           all=all,
+                           sentiment_general=sentiment_general,
                            sentiment_vaccination=sentiment_vaccination,
                            sentiment_restriction=sentiment_restriction,
-                           plot_general=plot_general)
-
-
+                           actual_scatter=actual_scatter,
+                           pie=pie)
 
 
 if __name__ == '__main__':
