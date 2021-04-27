ff6efd88db5015b24742851dd660767df6274a38
Fabio Palliparambil <fabiolearning1998@gmail.com>
2021-04-27 15:38:11 +0100

    a

--- hashtag.py
+++ hashtag.py
@@ -1,4 +1,6 @@
 import json
+import string
+
 import nltk
 import pandas as pd
 import plotly.graph_objects as go
@@ -29,38 +31,31 @@ def collectHashtag(x):
 Function for collecting hashed into multiple dataframe
 """
 
+
+# def hastag_dataframe(df):
+#     HT_general = collectHashtag(df['text'])
+#     HT_general = sum(HT_general, [])
+#     general_tag = nltk.FreqDist(HT_general)
+#     hashtag_general = pd.DataFrame({'Hashtag': list(general_tag.keys()), 'Count': list(general_tag.values())})
+#     hashtag_general = hashtag_general.nlargest(columns='Count', n=10)
+#     return hashtag_general
+
+def clean_hastag(text):
+    text = "".join([char for char in text if char not in string.punctuation])
+    text = text.lower()
+    return text
+
+
 def hastag_dataframe(df):
     HT_general = collectHashtag(df['text'])
     HT_general = sum(HT_general, [])
     general_tag = nltk.FreqDist(HT_general)
     hashtag_general = pd.DataFrame({'Hashtag': list(general_tag.keys()), 'Count': list(general_tag.values())})
-    hashtag_general = hashtag_general.nlargest(columns='Count', n=10)
+    hashtag_general['Hashtag'] = hashtag_general['Hashtag'].apply(lambda x: clean_hastag(x))
+    hashtag_general['Hashtag'] = hashtag_general['Hashtag'].drop_duplicates()
+    hashtag_general = hashtag_general.dropna()
+    hashtag_general = hashtag_general.nlargest(columns='Count', n=15)
     return hashtag_general
-
-#
-# def hashtag(df_general, df_restriction, df_vaccination):
-#
-#     HT_general = collectHashtag(df_general['text'])
-#     HT_general = sum(HT_general, [])
-#     general_tag = nltk.FreqDist(HT_general)
-#     hashtag_general = pd.DataFrame({'Hashtag': list(general_tag.keys()), 'Count': list(general_tag.values())})
-#     hashtag_general = hashtag_general.nlargest(columns='Count', n=10)
-#
-#     HT_restriction = collectHashtag(df_restriction['text'])
-#     HT_restriction = sum(HT_restriction, [])
-#     restriction_tag = nltk.FreqDist(HT_restriction)
-#     hashtag_restriction = pd.DataFrame(
-#         {'Hashtag': list(restriction_tag.keys()), 'Count': list(restriction_tag.values())})
-#     hashtag_restriction = hashtag_restriction.nlargest(columns='Count', n=10)
-#
-#     HT_vaccination = collectHashtag(df_vaccination['text'])
-#     HT_vaccination = sum(HT_vaccination, [])
-#     vaccination_tag = nltk.FreqDist(HT_vaccination)
-#     hashtag_vaccination = pd.DataFrame(
-#         {'Hashtag': list(vaccination_tag.keys()), 'Count': list(vaccination_tag.values())})
-#     hashtag_vaccination = hashtag_vaccination.nlargest(columns='Count', n=10)
-#
-#     return hashtag_general, hashtag_restriction, hashtag_vaccination
 
 
 df_general_hash_tag = hastag_dataframe(df_general)
@@ -68,13 +63,7 @@ df_restriction_hash_tag = hastag_dataframe(df_restriction)
 df_vaccination_hash_tag = hastag_dataframe(df_vaccination)
 
 
-# df_general_hash_tag, df_restriction_hash_tag, df_vaccination_hash_tag = hashtag(df_general, df_restriction,
-#                                                                                 df_vaccination)
-
-
 def create_plot(df_general_hashtag, df_restriction_hashtag, df_vaccination_hashtag):
-
-
     scatter_general = [go.Bar(
         x=df_general_hashtag['Hashtag'],
         y=df_general_hashtag['Count'],
@@ -169,10 +158,9 @@ def create_plot(df_general_hashtag, df_restriction_hashtag, df_vaccination_hasht
         y=df_general_hashtag['Count'],
         mode='lines+markers',
         name='lines+markers',
-        marker_color = 'rgba(255, 182, 193, .9)'
+        marker_color='rgba(255, 182, 193, .9)'
         # mode='markers'
     )]
-
 
     """ Scatter """
 
@@ -182,7 +170,7 @@ def create_plot(df_general_hashtag, df_restriction_hashtag, df_vaccination_hasht
         y=df_restriction_hashtag['Count'],
         mode='lines+markers',
         name='lines+markers',
-        marker_color = 'rgba(255, 182, 193, .9)'
+        marker_color='rgba(255, 182, 193, .9)'
         # mode='markers'
     )]
 
@@ -194,7 +182,7 @@ def create_plot(df_general_hashtag, df_restriction_hashtag, df_vaccination_hasht
         y=df_vaccination_hashtag['Count'],
         mode='lines+markers',
         name='lines+markers',
-        marker_color = 'rgba(255, 182, 193, .9)'
+        marker_color='rgba(255, 182, 193, .9)'
         # mode='markers'
     )]
 
@@ -221,14 +209,12 @@ def create_plot(df_general_hashtag, df_restriction_hashtag, df_vaccination_hasht
             name='COVID-19 Vaccination Hashtag'
         ))
 
-
-
     Scatters = go.Figure(data=go.Scatter(
         x=df_general_hashtag['Hashtag'],
         y=df_general_hashtag['Count'],
         mode='markers',
-        marker=dict(size=[40, 40, 40, 40,40,40,40,40,40,40],
-                    color=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        marker=dict(size=[100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30],
+                    color=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
     ))
 
     # labels = ['Oxygen', 'Hydrogen', 'Carbon_Dioxide', 'Nitrogen']
@@ -251,5 +237,4 @@ def create_plot(df_general_hashtag, df_restriction_hashtag, df_vaccination_hasht
     Scatters_JSON = json.dumps(Scatters, cls=plotly.utils.PlotlyJSONEncoder)
     Pie_json = json.dumps(Pie, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return general_JSON, restriction_JSON, vaccination_JSON,line_genreal_JSON,line_restriction_JSON,line_vaccination_JSON,all_scatter_lineJSON,Scatters_JSON,Pie_json
-
+    return general_JSON, restriction_JSON, vaccination_JSON, line_genreal_JSON, line_restriction_JSON, line_vaccination_JSON, all_scatter_lineJSON, Scatters_JSON, Pie_json
